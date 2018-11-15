aab66e751e712fdde98544397a0f9cdb46ae06d6
TJesT <42508019+TJesT@users.noreply.github.com>
2018-11-15 23:09:06 +0700

    Updated fork constant string variables from local

--- Main.py
+++ Main.py
@@ -3,10 +3,13 @@ Developers:
 Sychov A. (), Virt A. (), Ostanina K. ()
 
 """
-print("Укажите ваш семейный статус:")
-print("(1) Один субъект")
-print("(2) Семейная пара")
-print("(3) Родитель-одиночка")
+import local
+
+print(local.RU_INPUT_UR_FAMILY_CATEGORY)
+print(local.RU_ONLY_ONE)
+print(local.RU_PARENTS)
+print(local.RU_SINGLE_PARENT)
 status = int(input())
-print("Укажите ваш ежемесячный доход (в долларах):")
+
+print(local.RU_INPUT_UR_MONTHLY_IN_DOLLARS)
 salary = int(input()) * 12
