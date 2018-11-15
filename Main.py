302b5fa548ab582af871cae7f0455703e226def3
SalilusS <44485874+SalilusS@users.noreply.github.com>
2018-11-15 23:51:10 +0700

    Update Main.py
    Изменение inputs. Ежемесячный расчёт.

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ Sychov A. (), Virt A. (), Ostanina K. ()
 """
 import local
 
-print(local.RU_INPUT_UR_FAMILY_CATEGORY)
+"""print(local.RU_INPUT_UR_FAMILY_CATEGORY)
 print("(1) " + local.RU_ONLY_ONE)
 print("(2) " + local.RU_PARENTS)
 print("(3) " + local.RU_SINGLE_PARENT)
@@ -13,3 +13,12 @@ status = int(input())
 
 print(local.RU_INPUT_UR_MONTHLY_IN_DOLLARS)
 salary = int(input()) * 12
+"""
+names_months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+                'november', 'december']
+annual_salary = 0
+for month in range(12):
+    print("Ваша зарплата в", names_months[month], "(В долларах):")
+    salary = int(input())
+    annual_salary += salary
+print(annual_salary)
