3e73711a7154fa55c2613b4cc4967a746e7b0e6e
TJesT <42508019+TJesT@users.noreply.github.com>
2018-11-15 22:55:38 +0700

    Main inputs ready

--- Main.py
+++ Main.py
@@ -3,7 +3,10 @@ Developers:
 Sychov A. (), Virt A. (), Ostanina K. ()
 
 """
-#Well...
-
-#TODOS:
-#TODO: Analysis
+print("Укажите ваш семейный статус:")
+print("(1) Один субъект")
+print("(2) Семейная пара")
+print("(3) Родитель-одиночка")
+status = int(input())
+print("Укажите ваш ежемесячный доход (в долларах):")
+salary = int(input())
