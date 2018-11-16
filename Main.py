1ee08e646924bc021aecbfa234148597b4644f02
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2018-11-16 09:28:54 +0700

    Connected with local... again

--- Main.py
+++ Main.py
@@ -5,19 +5,29 @@ Sychov A., Virt A., Ostanina K.
 """
 import local
 
+NAME_OF_MONTHS = [
+  local.RU_MONTH_JANUARY, 
+  local.RU_MONTH_FEBRUARY, 
+  local.RU_MONTH_MARCH,
+  local.RU_MONTH_APRIL,
+  local.RU_MONTH_MAY, 
+  local.RU_MONTH_JUNE, 
+  local.RU_MONTH_JULY, 
+  local.RU_MONTH_AUGUST, 
+  local.RU_MONTH_SEPTEMBER, 
+  local.RU_MONTH_OCTOBER, 
+  local.RU_MONTH_NOVEMBER, 
+  local.RU_MONTH_DECEMBER
+]
+
 print(local.RU_INPUT_UR_FAMILY_CATEGORY)
 print("(1) " + local.RU_ONLY_ONE)
 print("(2) " + local.RU_PARENTS)
 print("(3) " + local.RU_SINGLE_PARENT)
 status = int(input())
 
-"""print(local.RU_INPUT_UR_MONTHLY_IN_DOLLARS)
-salary = int(input()) * 12
-"""
-names_months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
-                'november', 'december']
 annual_salary = 0
-for month in range(12):
-    print("Ваша зарплата в", names_months[month], "(В долларах):")
+for month in NAME_OF_MONTHS:
+    print(local.RU_INPUT_UR_TAX, month, local.RU_IN_DOLLARS)
     salary = float(input())
     annual_salary += salary
